afd138d12c3fe7221a9ef50fcf0d20664732edbf
Lisi <lisiw@clemson.edu>
2020-07-13 15:34:52 -0400

    inheritance

--- flaskapp.py
+++ flaskapp.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [{
+    'author': 'Lisi Wang',
+    'title': "blog 2"
+}]
 
 ### the two routes are beening handler by the same function
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1> Home Page </h1>"
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-    return "<h1> About Page </h1>"
+    return render_template('about.html', title = 'About')
 
 if __name__ == "__main__":
     app.run(debug = True)
